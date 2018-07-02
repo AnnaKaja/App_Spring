546f8fb026464966c7d734734033a9bad389d62b
anna kaja <anna_kaja@op.pl>
2018-07-02 17:21:23 +0200

    Initial commit

---
+++ src/main/java/com/pokemon/service/PokemonService.java
@@ -0,0 +1,9 @@
+package com.pokemon.service;
+
+import com.pokemon.dto.PokemonDto;
+
+import java.io.IOException;
+
+public interface PokemonService {
+    PokemonDto getPokemonDto(String id) throws IOException;
+}
